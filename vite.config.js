89a4efaaea85d6a774ce9ec16f750783fff2899d
Suriya <IS1160@ISOURCE.LOCAL>
2025-09-23 12:07:28 +0530

    Login

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],   // split react core
+          vendor: ['lottie-web'],          // split lottie
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1500, // increase limit from 500kb → 1.5mb
+  },
 })
